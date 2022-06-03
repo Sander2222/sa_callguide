7269a24267f25eae0e47451a1c1363e6e9a19d57
Sander <86840128+Sander2222@users.noreply.github.com>
2022-06-03 16:26:28 +0200

    add config: Groups, Translate, Button, Wait

--- config.lua
+++ config.lua
@@ -1,10 +1,19 @@
 Config = {}
 
-Config.CallCoords = { x =  -691.86999511719, y = -2383.1762695313, z = 13.891237258911 }
-Config.Button = 38
+Config.CallCoords = { x= -1057.0100097656,y= -2765.6240234375,z= 4.6396818161011 }
+Config.Button = 38 -- Button is E for now (https://docs.fivem.net/docs/game-references/controls)
+Config.WaitForTeam = 3 -- This time must wait the player to call a teammeber after called a teammember (in Minutes)
+Config.Groups = {
+    'mod',
+    'admin',
+    'superadmin'
+}
+
 
 Language = {
-    ['PressButton'] = 'Press ~INPUT_CONTEXT~ to call a teammember',
-    ['CalledAdmin'] = 'You called a admin',
-    ['PlayerAsked'] = 'This player asked for a teammeber: '
+    ['PressButton'] = 'Press ~INPUT_CONTEXT~ to call a teammember.',
+    ['CalledAdmin'] = 'You called a admin for ingame whitelist.',
+    ['PlayerAsked'] = 'This player asked for a teammember: ',
+    ['WaitTime'] = 'You need to wait this time befor you can call a nother: ',
+    ['Minutes'] = ' minutes'
 }
